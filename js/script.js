10f5c4278439ee458296c818a1658f4127844521
KoguroRisa <mochipeco15@gmail.com>
2024-03-10 15:41:47 +0900

    フェードイン機能追加

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
 $(function () {
 
+  //フェードイン
+  $(function () {
+    // ウインドウをスクロールしたら
+    $(window).scroll(function () {
+      // .js-fadeのクラスを持つ要素（それぞれに対して）
+      $('.js-fade').each(function () {
+        // この要素のスクロール位置（上から）
+        var pos = $(this).offset().top;
+        // ウインドウのスクロール量（上から）
+        var scroll = $(window).scrollTop();
+        // ウインドウの縦幅
+        var windowHeight = $(window).height();
+        // ウインドウのスクロール量（上から）が
+        // この要素のスクロール位置 - ウインドウの縦幅 + 100pxより大きい場合
+        if (scroll > pos - windowHeight + 100) {
+          // .scrollというクラス.js-fadeに付与する
+          $(this).addClass('scroll');
+        }
+      });
+    });
+  });
+
   //ページ内スクロール
   var navHeight = $(".header").outerHeight();
 
