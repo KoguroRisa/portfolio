2be77956f0b44d7bb9893e9561ee9c3863e12fc9
KoguroRisa <mochipeco15@gmail.com>
2024-03-10 21:22:14 +0900

    contact,ヘッダー修正

--- js/script.js
+++ js/script.js
@@ -68,11 +68,6 @@ $(function () {
     ScrollTimelineAnime();// 線が伸びる関数を呼ぶ
   });
 
-  // ページが読み込まれたらすぐに動かしたい場合の記述
-  $(window).on('load', function () {
-    ScrollTimelineAnime();// 線が伸びる関数を呼ぶ
-  });
-
   //タイピングっぽい動き
   const typewriter = (param) => {
     let el = document.querySelector(param.el);
@@ -113,6 +108,25 @@ $(function () {
       $('.tabBlock:nth-child(' + (index + 1) + ')').removeClass('tabBlock-none');
     });
   }
+});
+
+// 左に動くアニメーション
+$(window).on('load', function () {
+  $('.leftAnime').each(function () {
+    var elemPos = $(this).offset().top - 50;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      //左から右へ表示するクラスを付与
+      //テキスト要素を挟む親要素（左側）とテキスト要素を元位置でアニメーションをおこなう
+      $(this).addClass("slideAnimeLeftRight"); //要素を左枠外にへ移動しCSSアニメーションで左から元の位置に移動
+      $(this).children(".leftAnimeInner").addClass("slideAnimeRightLeft");  //子要素は親要素のアニメーションに影響されないように逆の指定をし元の位置をキープするアニメーションをおこなう
+    } else {
+      //左から右へ表示するクラスを取り除く
+      $(this).removeClass("slideAnimeLeftRight");
+      $(this).children(".leftAnimeInner").removeClass("slideAnimeRightLeft");
+    }
+  });
 });
 
 // //ローディング画面の表示
