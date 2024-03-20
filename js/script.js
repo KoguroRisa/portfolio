b75b5b3ed7c71f6b17ba345a923ff43cc026ecb4
KoguroRisa <mochipeco15@gmail.com>
2024-03-20 18:55:37 +0900

    アニメーション修正

--- js/script.js
+++ js/script.js
@@ -67,6 +67,25 @@ $(function () {
     ScrollTimelineAnime();// 線が伸びる関数を呼ぶ
   });
 
+  // smoothTriggerにsmoothTextAppearというクラス名を付ける定義
+  function SmoothTextAnime() {
+    $('.smoothTextTrigger').each(function () { //smoothTextTriggerというクラス名が
+      var elemPos = $(this).offset().top - 50;//要素より、50px上の
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll >= elemPos - windowHeight) {
+        $(this).addClass('smoothTextAppear');// 画面内に入ったらsmoothTextAppearというクラス名を追記
+      } else {
+        $(this).removeClass('smoothTextAppear');// 画面外に出たらsmoothTextAppearというクラス名を外す
+      }
+    });
+  }
+
+  // 画面をスクロールをしたら動かしたい場合の記述
+  $(window).scroll(function () {
+    SmoothTextAnime();/* アニメーション用の関数を呼ぶ*/
+  });// ここまで画面をスクロールをしたら動かしたい場合の記述
+
   //タイピングっぽい動き
   const typewriter = (param) => {
     let el = document.querySelector(param.el);
@@ -108,17 +127,17 @@ $(function () {
     });
   }
 
-  //タイトルアニメーション
-  const titleVisible = "-visible";
-  const TIMEOUT = 1900;
-  const $target = $(".title");
+  //   //タイトルアニメーション
+  //   const titleVisible = "-visible";
+  //   const TIMEOUT = 1900;
+  //   const $target = $(".title");
 
-  setInterval(() => {
-    $target.addClass(titleVisible);
-    setTimeout(() => {
-      $target.removeClass(titleVisible);
-    }, TIMEOUT);
-  }, TIMEOUT * 2);
+  //   setInterval(() => {
+  //     $target.addClass(titleVisible);
+  //     setTimeout(() => {
+  //       $target.removeClass(titleVisible);
+  //     }, TIMEOUT);
+  //   }, TIMEOUT * 2);
 });
 
 // 左に動くアニメーション
@@ -139,6 +158,8 @@ $(window).on('load', function () {
     }
   });
 });
+
+
 
 
 // //ローディング画面の表示
